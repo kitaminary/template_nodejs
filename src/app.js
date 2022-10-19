2857e6cf4060eb7ce025094dd30a3000f46c54ba
Yaroslav Kvasnytsia <qwerty300067@gmail.com>
2022-10-19 15:42:00 +0300

    nodejs template

---
+++ src/app.js
@@ -0,0 +1,13 @@
+
+
+// get info about your system
+import os from 'os';
+// for working with filing system
+import fs from 'fs'
+// for get path of file
+import path from 'path';
+// for create server
+import http from 'http'
+
+const curretnPATH = import.meta.url
+console.log(curretnPATH);
