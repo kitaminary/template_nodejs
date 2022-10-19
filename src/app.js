93d8157229b53616d13fc24e975191e044d5131d
Yaroslav Kvasnytsia <qwerty300067@gmail.com>
2022-10-19 16:24:34 +0300

    add eslint and prettier

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
-
-
 // get info about your system
-import os from 'os';
+import os from "os";
 // for working with filing system
-import fs from 'fs'
+import fs from "fs";
 // for get path of file
-import path from 'path';
+import path from "path";
 // for create server
-import http from 'http'
+import http from "http";
 
-const curretnPATH = import.meta.url
-console.log(curretnPATH);
+const curretnPathOfFile = import.meta.url;
+console.log(curretnPathOfFile);
+
+/*
+to start debugging
+Run -> add config -> NodeJS
+and  press F5
+*/
+// step of debugging F10
